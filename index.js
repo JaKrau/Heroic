f425587529ffe001e20eb297d4c7c6d255a19afc
JaKrau <jakeziah@gmail.com>
2023-08-31 11:33:27 -0700

    Add validators for text and color input

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
 const inquirer = require('inquirer');
-const { Triangle, Circle, Square } = require('./lib/shape.js')
-
+const { Triangle, Circle, Square } = require('./lib/shape.js');
+const { logoTextLength, logoColorValidator, writeToFile } = require('./lib/validators.js');
 
 function init() {
     inquirer.prompt([
         {
             type: 'input',
-            message: 'Enter three characters for logo text:',
+            message: 'Enter up to three characters for logo text:',
             name: 'text',
+            validate: logoTextLength
         },
         {
             type: 'input',
-            message: 'Enter a color, by keyword or hexadecimal code, for text color:',
+            message: 'Enter a color by name (using lowercase characters) for text color:',
             name: 'textColor',
+            validate: logoColorValidator
         },
         {
             type: 'list',
@@ -24,6 +26,7 @@ function init() {
             type: 'input',
             message: 'Enter a color, by keyword or hexadecimal code, for shape color:',
             name: 'shapeColor',
+            validate: logoColorValidator
         },
     ]).then((response) => {
         console.log(response);
@@ -31,10 +34,19 @@ function init() {
         switch(response.shape) {
             case 'Triangle':
                 const triangle = new Triangle(response.text, response.textColor, response.shape, response.shapeColor);
+                const renderTriangle = triangle.renderSVG();
+                writeToFile('./examples/logo.svg', renderTriangle);
+                break;
             case 'Circle':
                 const circle = new Circle(response.text, response.textColor, response.shape, response.shapeColor);
+                const renderCircle = circle.renderSVG();
+                writeToFile('./examples/logo.svg', renderCircle);
+                break;
             case 'Square':
                 const square = new Square(response.text, response.textColor, response.shape, response.shapeColor);
+                const renderSquare = square.renderSVG();
+                writeToFile('./examples/logo.svg', renderSquare);
+                break;
         }
     })
 }
